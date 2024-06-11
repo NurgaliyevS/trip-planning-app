19fc8a7ee498a4730905074fc8c55dcc24c4c01d
NURGASAB <sabyr.nurgaliyev@bcc.kz>
2024-06-11 16:04:57 +0500

    upd

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,18 @@ export default function Home() {
     <main
       className={`${nunito.variable} flex flex-col min-h-screen p-2 lg:p-6`}
     >
-      <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4 flex flex-col gap-3 items-center lg:items-start">
-        <span>
-          Don't call it a dream <br />
-        </span>
-        <span>Call it a plan</span>
-      </h1>
+      <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
+        <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4 flex flex-col gap-3 items-center lg:items-start">
+          <span>
+            Don't call it a dream <br />
+          </span>
+          <span>Call it a plan</span>
+        </h1>
+        <p className="text-lg opacity-80 leading-relaxed">
+          Plan your trip easily with all details in one place. <br />
+          Keep track of cities, dates, times, and notes.
+        </p>
+      </div>
     </main>
   );
 }
