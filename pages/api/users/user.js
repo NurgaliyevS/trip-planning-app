b55f24ace5385944f3389ec2f2c74b5850fe712b
NURGASAB <sabyr.nurgaliyev@bcc.kz>
2024-06-13 17:11:09 +0500

    user upd

--- pages/api/users/user.js
+++ pages/api/users/user.js
@@ -11,46 +11,45 @@ export default async function handler(req, res) {
       try {
         // Check if a user ID is provided in the request URL
         const userId = req.query.id;
-        console.log(userId, "user");
         if (userId) {
           // Find the user with the provided ID
           const user = await User.findById(userId);
-          console.log(user, 'findby id');
-
           if (!user) {
-            res.status(404).json({ success: false, message: "User not found" });
-            return;
+            return res
+              .status(404)
+              .json({ success: false, message: "User not found" });
           }
-          res
+          return res
             .status(200)
             .json({ success: true, message: "User found", data: user });
         } else {
-          // Get all users
           const users = await User.find();
-          res
+          return res
             .status(200)
             .json({ success: true, message: "Users found", data: users });
         }
       } catch (error) {
-        res
+        if (error.kind === "ObjectId") {
+          return res
+            .status(404)
+            .json({ success: false, message: "User not found" });
+        }
+        return res
           .status(500)
           .json({ success: false, message: "Internal server error" });
       }
-      break;
     case "POST":
       try {
         const user = await User.create(req.body);
-        res.status(201).json({
+        return res.status(201).json({
           success: true,
           message: "User created successfully",
           data: user,
         });
       } catch (error) {
-        res.status(400).json({ success: false });
+        return res.status(400).json({ success: false, message: "Bad request" });
       }
-      break;
     default:
-      res.status(400).json({ success: false });
-      break;
+      return res.status(400).json({ success: false, message: "Bad request" });
   }
 }
