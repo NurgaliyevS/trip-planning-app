e2cfddfbf3213b3e74ca2e6b94509f2715e62e39
NURGASAB <sabyr.nurgaliyev@bcc.kz>
2024-06-13 17:59:18 +0500

    upd

---
+++ components/Main.jsx
@@ -0,0 +1,20 @@
+function Main() {
+  return (
+    <section className="max-w-7xl flex flex-col lg:flex-row gap-16 lg:gap-20 lg:items-start px-8 py-8 lg:py-20">
+      <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
+        <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4 flex flex-col gap-3 items-center lg:items-start">
+          <span>
+            Don't call it a dream <br />
+          </span>
+          <span>Call it a plan</span>
+        </h1>
+        <p className="text-lg opacity-80 leading-relaxed">
+          Plan your trip easily with all details in one place. <br />
+          Keep track of cities, dates, times, and notes.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Main;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,17 @@
 import { Nunito } from "next/font/google";
-import GoogleButton from "@/components/google-button";
+import GoogleButton from "@/components/GoogleButton";
+import Main from "@/components/Main";
+import Navbar from "@/components/Navbar";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`${nunito.variable} flex flex-col min-h-screen p-2 lg:p-6`}
+      className={`${nunito.className} flex flex-col min-h-screen p-2 lg:p-6 mx-auto`}
     >
-      <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
-        <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4 flex flex-col gap-3 items-center lg:items-start">
-          <span>
-            Don't call it a dream <br />
-          </span>
-          <span>Call it a plan</span>
-        </h1>
-        <p className="text-lg opacity-80 leading-relaxed">
-          Plan your trip easily with all details in one place. <br />
-          Keep track of cities, dates, times, and notes.
-        </p>
-      </div>
-
-      <GoogleButton />
+      <Navbar />
+      <Main />
     </main>
   );
 }
