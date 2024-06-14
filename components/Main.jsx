c4e454c329718bc51d84a1b98fd135e20489cdd4
NurgaliyevS <nurgaliyev000@gmail.com>
2024-06-14 12:31:09 +0500

    upd

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,17 +1,25 @@
+import Link from "next/link";
+
 function Main() {
   return (
-    <section className="max-w-7xl flex flex-col lg:flex-row gap-16 lg:gap-20 lg:items-start px-8 py-8 lg:py-20">
-      <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
-        <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4 flex flex-col gap-3 items-center lg:items-start">
-          <span>
-            Don't call it a dream <br />
-          </span>
-          <span>Call it a plan</span>
+    <section className="max-w-5xl mx-auto  flex flex-col items-center justify-center gap-16 lg:gap-20 px-8 py-12 lg:py-32">
+      <div className="flex flex-col gap-10 lg:gap-12 items-center justify-center text-center">
+        <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4">
+          Don't call it a dream <br />
+          Call it a plan
         </h1>
-        <p className="text-lg opacity-80 leading-relaxed">
+        <p className="text-lg text-base-content-secondary leading-relaxed max-w-md mx-auto">
           Plan your trip easily with all details in one place. <br />
-          Keep track of cities, dates, times, and notes.
         </p>
+
+        <Link
+          href="#buy"
+          role="button"
+          className="btn btn-error group btn-wide"
+          title="BUY NOW"
+        >
+          BUY NOW
+        </Link>
       </div>
     </section>
   );
