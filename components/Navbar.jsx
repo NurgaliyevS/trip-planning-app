ac21b1b9dd015cf94885e65bc7d0aa7f0c6bef19
NurgaliyevS <nurgaliyev000@gmail.com>
2024-06-14 11:58:04 +0500

    navbar done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,114 +3,82 @@ import Link from "next/link";
 import { useState } from "react";
 
 function Navbar() {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
 
   return (
-    <div className="relative z-[99999]">
-      <header className="z-10 relative bg-transparent">
-        <nav className="container max-w-5xl flex items-center justify-between px-8 py-4 mx-auto">
-          <div className="w-1/2 justify-start flex gap-2">
-            <Link
-              href="/"
-              className="flex items-center gap-2 shrink-0"
-              title="Homepage"
-            >
-              <Image src={"/favicon.ico"} width={32} height={32} alt="Icon" />
-              <span className="font-extrabold text-lg">Trip Plans</span>
-            </Link>
-          </div>
+    <header className="container max-w-5xl flex items-center flex-wrap justify-between px-8 py-4 mx-auto">
+      <div className="hidden lg:flex items-center gap-2">
+        <Link href="/" className="flex items-center" title="Homepage">
+          <Image src={"/favicon.ico"} width={32} height={32} alt="Icon" />
+          <span className="font-extrabold text-lg ml-2">Trip Plans</span>
+        </Link>
+      </div>
 
-          <div className="hidden lg:flex lg:justify-center lg:gap-12 lg:items-center">
+      <div className="hidden lg:flex lg:justify-center lg:gap-12 lg:items-center">
+        <a className="link link-hover" title="Pricing" href="/#pricing">
+          Pricing
+        </a>
+        <a className="link link-hover" title="FAQ" href="/#faq">
+          FAQ
+        </a>
+        <button className="btn btn-sm ml-4">Login</button>
+      </div>
+
+      <div className="lg:hidden w-full mt-4 flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Link href="/" className="flex items-center" title="Homepage">
+            <Image src={"/favicon.ico"} width={32} height={32} alt="Icon" />
+            <span className="font-extrabold text-lg ml-2">Trip Plans</span>
+          </Link>
+        </div>
+        <button
+          className="flex items-center px-3 py-2 hover:text-black"
+          onClick={toggleDropdown}
+        >
+          <svg
+            className="fill-current h-4 w-4"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+
+      <div
+        className={`${showDropdown ? "block" : "hidden"} lg:hidden w-full mt-2`}
+      >
+        <div className="text-sm flex flex-col gap-3 py-4">
+          <div className="hidden lg:flex lg:justify-center lg:gap-12 lg:items-center lg:w-1/2">
             <a className="link link-hover" title="Pricing" href="/#pricing">
               Pricing
             </a>
             <a className="link link-hover" title="FAQ" href="/#faq">
               FAQ
             </a>
+            <button className="btn btn-sm ml-4">Login</button>
           </div>
 
-          <div className="flex lg:hidden">
-            <button
-              onClick={() => setShowMenu(!showMenu)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
-            >
-              <span className="sr-only">Open Menu</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 text-base-content"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                ></path>
-              </svg>
-            </button>
-          </div>
-
-          <div className="hidden lg:flex w-1/2 justify-end">
-            <button className="btn btn-sm">Login</button>
-          </div>
-        </nav>
-      </header>
-
-      <div
-        className={`fixed inset-0 bg-base-200 z-50 p-8 transition-transform transform ${
-          showMenu ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <nav className="container max-w-5xl mx-auto flex flex-col justify-between h-full">
-          <div className="flex items-center justify-between">
-            <Link
-              href="/"
-              className="flex items-center gap-2 shrink-0"
-              title="Homepage"
-            >
-              <Image src={"/favicon.ico"} width={32} height={32} alt="Icon" />
-              <span className="font-extrabold text-lg">Trip Plans</span>
-            </Link>
-            <button
-              onClick={() => setShowMenu(!showMenu)}
-              className="-m-2.5 rounded-md p-2.5"
-            >
-              <span className="sr-only">Close menu</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div className="mt-6 flex-grow">
-            <div className="flex flex-col gap-y-4 items-start">
-              <a className="link link-hover" title="Pricing" href="/#pricing">
-                Pricing
-              </a>
-              <a className="link link-hover" title="FAQ" href="/#faq">
-                FAQ
-              </a>
-            </div>
-            <div className="border-b my-4"></div>
-            <div className="flex flex-col gap-4">
-              <button className="btn btn-sm">Login</button>
-            </div>
-          </div>
-        </nav>
+          <a
+            title="Pricing"
+            href="/#pricing"
+            className="block mt-2 hover:text-white"
+          >
+            Pricing
+          </a>
+          <a title="FAQ" href="/#faq" className="block mt-2 hover:text-white">
+            FAQ
+          </a>
+        </div>
+        <div className="border-b my-4"></div>
+        <button className="w-full btn btn-sm mt-4">Login</button>
       </div>
-    </div>
+    </header>
   );
 }
 
