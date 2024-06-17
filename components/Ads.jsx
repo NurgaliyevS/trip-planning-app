16cdd3de8cb1a7a815f1375e523474321199de16
NurgaliyevS <nurgaliyev000@gmail.com>
2024-06-17 17:50:28 +0500

    upd

--- components/Ads.jsx
+++ components/Ads.jsx
@@ -1,6 +1,164 @@
 function Ads() {
   return (
-    <section className="bg-slate-100 text-base-content min-h-screen p-2 lg:p-6">
+    <section className="bg-slate-100 text-base-content">
+      <div className="max-w-5xl mx-auto px-8 py-16 md:py-32 ">
+        <h2 className="text-center font-extrabold text-4xl md:text-5xl tracking-tight mb-12 md:mb-20">
+          Tired of planning trips?
+        </h2>
+        <div className="flex flex-col md:flex-row justify-center items-center md:items-start gap-8 md:gap-12">
+          <div className="bg-rose-100/75 text-rose-700 p-8 md:p-12 rounded-lg w-full max-w-md">
+            <h3 className="font-bold text-lg mb-4">
+              Journeys without TripPlanss
+            </h3>
+            <ul className="list-disc list-inside space-y-1.5">
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="w-4 h-4 shrink-0 opacity-75"
+                >
+                  <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z"></path>
+                </svg>
+                Hard to remember travel dates and times
+              </li>
+
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="w-4 h-4 shrink-0 opacity-75"
+                >
+                  <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z"></path>
+                </svg>
+                Can't easily track multiple destinations
+              </li>
+
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="w-4 h-4 shrink-0 opacity-75"
+                >
+                  <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z"></path>
+                </svg>
+                Important notes get lost
+              </li>
+
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="w-4 h-4 shrink-0 opacity-75"
+                >
+                  <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z"></path>
+                </svg>
+                Last-minute stress
+              </li>
+
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="w-4 h-4 shrink-0 opacity-75"
+                >
+                  <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z"></path>
+                </svg>
+                Travel anxiety
+              </li>
+            </ul>
+          </div>
+
+          <div className="bg-emerald-100/70 text-emerald-700 p-8 md:p-12 rounded-lg w-full max-w-md">
+            <h3 className="font-bold text-lg mb-4">Journeys with TripPlanss</h3>
+            <ul className="list-disc list-inside space-y-1.5">
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="w-4 h-4 shrink-0 opacity-75"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Remember all travel dates and times
+              </li>
+
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="w-4 h-4 shrink-0 opacity-75"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Track all destinations easily
+              </li>
+
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="w-4 h-4 shrink-0 opacity-75"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Keep all important notes in one place
+              </li>
+
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="w-4 h-4 shrink-0 opacity-75"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Stress-free trips
+              </li>
+
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="w-4 h-4 shrink-0 opacity-75"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Relaxed travel
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
