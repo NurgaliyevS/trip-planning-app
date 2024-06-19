48d25c5101f10d72318e1cf15353ed058762a408
NurgaliyevS <nurgaliyev000@gmail.com>
2024-06-19 21:11:51 +0500

    upd

--- backend/user.js
+++ backend/user.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema(
   {
     name: String,
-    email: String,
+    email: { type: String, unique: true },
     image: String,
     expires: String,
     access_token: String,
